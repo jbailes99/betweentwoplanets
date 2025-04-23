688af5177a6da660184419b24487816dd559f6e1
Jacob Bailes <jakebailes76@yahoo.com>
2025-04-23 12:01:50 -0400

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import Nav from './components/nav'
+import ViewportHeight from './components/viewport-height'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -26,8 +27,11 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Nav />
-        {children}
+        <ViewportHeight />
+        <div className='fullscreen-fix'>
+          <Nav />
+          {children}
+        </div>
       </body>
     </html>
   )
